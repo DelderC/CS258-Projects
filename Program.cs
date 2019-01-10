3bb143957842690b422005f317a6e4d38167707d
DelderC <40806806+DelderC@users.noreply.github.com>
2019-01-10 14:12:44 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ Programmer: Devin Elder    
+ Date: 1/10/2019
+ Description: This program will output my name, hometown, and my interest.
+*/ 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lines 20-22 print out my name, hometown, and interest respectively.
+            Console.WriteLine("Devin Elder");
+            Console.WriteLine("Reykjavik, Iceland");
+            Console.WriteLine("My hobby is browsing Reddit for longer than is necessary.");
+        }
+    }
+}
